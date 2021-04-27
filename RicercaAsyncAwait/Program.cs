c3990722e9b6d1bdbf4e666645c340fe73005016
Alessio Manieri <mani.alexander03@gmail.com>
2021-04-27 16:00:21 +0200

    concluso

--- RicercaAsyncAwait/Program.cs
+++ RicercaAsyncAwait/Program.cs
@@ -10,6 +10,44 @@ namespace RicercaAsyncAwait
     {
         static void Main(string[] args)
         {
+            int[] listanumeri = new int[100];
+            RiempiArray(ref listanumeri);
+
+            Console.WriteLine("Inserisci qui un numero da cercare:");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(RicercaNumeroAsync(numero1, listanumeri).Result);
+
+            Console.WriteLine("Inserisci qui un numero da cercare:");
+            int numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(RicercaNumeroAsync(numero2, listanumeri).Result);
+
+            Console.ReadLine();
         }
+
+        private static async Task<bool> RicercaNumeroAsync(int n, int[] array)
+        {
+            bool trovato = false;
+            await Task.Run(() =>
+            {
+                for (int c = 0; c < 100; c++)
+                {
+                    if (n == array[c])
+                    {
+                        trovato = true;
+                    }
+                }
+            });
+            return trovato;
+        }
+
+        private static void RiempiArray(ref int[] numeri)
+        {
+            Random r = new Random();
+            for (int c = 0; c < 100; c++)
+            {
+                numeri[c] = r.Next(0, 100);
+            }
+        }
+    }
     }
 }
